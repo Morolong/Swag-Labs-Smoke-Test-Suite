6adcda481acbd66f6d130634fc07d89e0a152485
Morolong <mg.morolong@gmail.com>
2026-04-15 13:40:37 +0200

    Added placeholder logic so that Cart function in Inventory.cs does not give a build error

--- Pages/Cart.cs
+++ Pages/Cart.cs
@@ -1,10 +1,16 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+using SeleniumExtras.WaitHelpers;
+using SmokeTestSuite.Core;
 
-namespace SmokeTestSuite.Pages
+namespace SmokeTestSuite.Pages;
+
+public class Cart : BasePage
 {
-    internal class Cart
-    {
-    }
+    private readonly By _cartItem = By.ClassName("cart_item");
+
+    public Cart(IWebDriver driver) : base(driver) { }
+
+    protected override void WaitForPageToLoad() =>
+      Wait.ForElementToBeVisible(_cartItem);
 }
