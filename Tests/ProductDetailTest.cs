f138a585944932fa1408433d4ac7f049b38d3a33
Morolong <mg.morolong@gmail.com>
2026-04-16 19:05:09 +0200

    Created this class.

---
+++ Tests/ProductDetailTest.cs
@@ -0,0 +1,43 @@
+﻿using NUnit.Framework;
+using SmokeTestSuite.Core;
+using SmokeTestSuite.Core.Config;
+using SmokeTestSuite.Pages;
+
+namespace SmokeTestSuite.Tests;
+
+[TestFixture]
+[Category("ProductDetail")]
+[Parallelizable(ParallelScope.All)]
+public class ProductDetailTest : BaseTest
+{
+    private ProductDetail _productDetail;
+
+    [SetUp]
+    public void ProductDetailSetUp()
+    {
+        var username = ConfigurationManager.Credentials.PositiveTestUser;
+        var password = ConfigurationManager.Credentials.LoginPassword;
+
+        var loginPage = new LoginPage(Driver!);
+        loginPage.Open();
+
+        var inventoryPage = loginPage.LoginAs(username, password);
+
+        _productDetail = inventoryPage.GoToProductDetail();
+    }
+
+    [Test]
+    [Category("Smoke")]
+    [Property("TestID", "SMK-004")]
+    [Description("Product Detail Loads Correctly")]
+    public void VerifyProductDetailPageElements()
+    {
+        Assert.That(_productDetail.IsAtPage(), Is.True,
+            "Expected to be on the cart page");
+
+        var productDetailPageElements = _productDetail.GetProductDetailPageElements();
+
+        Assert.That(productDetailPageElements, Is.Empty,
+            $"Elements not visible: {string.Join(", ", productDetailPageElements)}");
+    }
+}
