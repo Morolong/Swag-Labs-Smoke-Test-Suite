83cf13c7c1c59795a41b475346bfb03a58681ca1
Morolong <mg.morolong@gmail.com>
2026-04-17 19:16:20 +0200

    - Added menu & logout selectors. - Added Logout method

--- Pages/ProductInventory.cs
+++ Pages/ProductInventory.cs
@@ -18,8 +18,9 @@ public class ProductInventory : BasePage
     private readonly By _stockPrice = By.ClassName("inventory_item_price");
     private readonly By _addToCartButton = By.CssSelector("[id^='add-to-cart-']");
     private readonly By _removeButton = By.Id("remove-sauce-labs-backpack");
-    private readonly By _redTShirt = By.CssSelector("#item_3_img_link > img"); 
-
+    private readonly By _redTShirt = By.CssSelector("#item_3_img_link > img");
+    private readonly By _logOutOption = By.Id("logout_sidebar_link");
+    private readonly By _menu = By.Id("react-burger-menu-btn");
 
     public bool IsAtPage() => base.IsAtPage("/inventory.html");
 
@@ -60,5 +61,16 @@ public class ProductInventory : BasePage
     {
         Click(_redTShirt);
         return new ProductDetail(Driver);
+    }
+
+    public LoginPage Logout()
+    {
+        Click(_menu);
+
+        Wait.ForElementToBeClickable(_logOutOption);
+
+        Click(_logOutOption);
+
+        return new LoginPage(Driver);
     }
 }
