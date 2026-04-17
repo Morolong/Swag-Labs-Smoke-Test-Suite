cae150b3d996cc1071f8a627027a82e98d101a4c
Morolong <mg.morolong@gmail.com>
2026-04-17 19:13:02 +0200

    -First Commit. - Added Page Selectors. - Added method to iterate through page selectors and check that they did load. - Added method for returning to ProductInventory page.

--- Pages/CheckoutComplete.cs
+++ Pages/CheckoutComplete.cs
@@ -1,10 +1,45 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using OpenQA.Selenium;
+using SmokeTestSuite.Core;
 
-namespace SmokeTestSuite.Pages
+namespace SmokeTestSuite.Pages;
+
+public class CheckoutComplete : BasePage
 {
-    internal class CheckoutComplete
+    private readonly By _logo = By.ClassName("app_logo");
+    private readonly By _checkOutCart = By.Id("shopping_cart_container");
+    private readonly By _menu = By.Id("react-burger-menu-btn");
+    private readonly By _checkoutCompletePageTitle = By.ClassName("title");
+    private readonly By _pageContainer = By.Id("checkout_complete_container");
+    private readonly By _backHomeButton = By.Id("back-to-products");
+
+    public CheckoutComplete(IWebDriver driver) : base(driver) { }
+
+    protected override void WaitForPageToLoad() =>
+    Wait.ForElementToBeVisible(_pageContainer);
+
+    public bool IsAtPage() => base.IsAtPage("checkout-complete.html");
+
+    public IEnumerable<string> GetCheckOutCompleteElements()
     {
+        var elements = new Dictionary<string, By>
+        {
+            { "Logo",                                    _logo },
+            { "Checkout Cart",                           _checkOutCart },
+            { "Menu",                                    _menu },
+            { "Checkout: Complete",                      _checkoutCompletePageTitle },
+            { "Checkout Overview Page Container",         _pageContainer },
+            { "Back Home Button",                               _backHomeButton},
+        };
+
+        return elements
+            .Where(e => !IsElementVisible(e.Value))
+            .Select(e => e.Key);
     }
+
+    public ProductInventory ClickBackHomeButton()
+    {
+        Click(_backHomeButton);
+        return new ProductInventory(Driver);
+    }
+
 }
